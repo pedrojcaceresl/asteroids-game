3425ac1aa739f76161d81dacd1a7414da49ad430
pedro.caceres <pedro.jcaceresl@gmail.com>
2025-09-07 12:41:49 -0300

    Add shooting functionality to Player class with cooldown management

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from circleshape import CircleShape
+from shot import Shot
 from constants import *
 import pygame
 
@@ -7,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
     
     
     def triangle(self):
@@ -29,7 +31,8 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
-
+        self.timer -= dt
+        
         if keys[pygame.K_a]:
             self.rotate(-dt)
 
@@ -41,5 +44,16 @@ class Player(CircleShape):
         
         if keys[pygame.K_s]:
             self.move(-dt)
+            
+        if keys[pygame.K_SPACE]:
+            self.shoot()
 
+    def shoot(self):
+        if self.timer > 0:
+            return
         
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = forward * PLAYER_SHOOT_SPEED
+        
+        self.timer = PLAYER_SHOOT_COOLDOWN
