1afd09bf101441555c30b01dbdf3b6b746140fe4
pedro.caceres <pedro.jcaceresl@gmail.com>
2025-09-07 12:40:46 -0300

    Add Asteroid class for asteroid representation and behavior in game

---
+++ asteroid.py
@@ -0,0 +1,32 @@
+from circleshape import CircleShape
+from constants import *
+import random
+import pygame
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (self.position.x, self.position.y), self.radius, 2)
+        
+    def update(self, dt):
+        self.position += self.velocity * dt
+        
+    def split(self):
+        self.kill()
+        
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # spawn two new asteroids
+        random_angle = random.uniform(20, 50)
+        vect1 = self.velocity.rotate(random_angle)
+        vect2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid1.velocity = vect1 * 1.2
+        asteroid2.velocity = vect2 * 1.2
