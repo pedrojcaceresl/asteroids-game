b21e5142423b2a453ef017f04294f18765f9d31c
pedro.caceres <pedro.jcaceresl@gmail.com>
2025-09-07 00:10:06 -0300

    Refactor main game loop to use sprite groups for updating and drawing

--- main.py
+++ main.py
@@ -11,18 +11,29 @@ def main():
     
     clock = pygame.time.Clock()
     dt = 0
+    
+    
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    
+    Player.containers = (updatables, drawables)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        
         screen.fill(color="black")
-        player.update(dt)
-        player.draw(screen)
+        
+        updatables.update(dt)
+        
+        for drawable in drawables:
+            drawable.draw(screen)
         
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
         
 if __name__ == "__main__":
     main()
